c05843d770a38361ed27416cc7dd84d749fc995c
Basil Akavity <vakavity@gmail.com>
2023-06-02 23:58:50 +0300

    first sketch

---
+++ src/test/java/OnlinerFormTest.java
@@ -0,0 +1,47 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class OnlinerFormTest {
+
+    @Test
+    public void checkRegistrationOnliner() {
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver", "G:\\Installation\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        driver.get("https://profile.onliner.by/registration");
+
+//        WebElement enterButton = driver.findElement(By.xpath("//div[@class=\"auth-bar__item auth-bar__item--text\"]"));
+//        enterButton.click();
+
+        WebElement emailField = driver.findElement(By.xpath("//input[@type=\"email\"]"));
+        emailField.sendKeys("wipilet483@onlcool.com");
+
+        WebElement passwordField = driver.findElement(By.xpath("//input[@placeholder=\"Придумайте пароль\"]"));
+        passwordField.sendKeys("Ed9KDd55jWHW6k");
+
+        WebElement repeatPassword = driver.findElement(By.xpath("//input[@placeholder=\"Повторите пароль\"]"));
+        repeatPassword.sendKeys("Ed9KDd55jWHW6k");
+
+        WebElement consentButton = driver.findElement(By.xpath("//span[@class=\"i-checkbox__faux\"]"));
+        consentButton.click();
+
+        WebElement submitButton = driver.findElement(By.xpath("//button[@type=\"submit\"]"));
+        submitButton.click();
+
+        WebElement goToMailButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href=\"http://mail.onlcool.com/\"]")));
+
+        Assert.assertTrue(goToMailButton.getText().contains("Перейти в почту"));
+
+        driver.quit();
+    }
+}
