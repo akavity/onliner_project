9420de8abb10b99daf014ba6cdd2b6a0485de68b
Basil Akavity <vakavity@gmail.com>
2023-06-03 22:12:42 +0300

    add BaseTest

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,17 @@
+import org.example.driver.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+
+    private WebDriver driver;
+    @BeforeMethod
+    public void init() {
+       driver = DriverManager.getDriver();
+    }
+    @AfterMethod
+    public void tearDown() {
+        DriverManager.closeDriver();
+    }
+}
