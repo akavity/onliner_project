8b3dcc575de8fb2bc3830abd77f465ca0b5540d2
Basil Akavity <vakavity@gmail.com>
2023-06-03 17:18:28 +0300

    add OnlinerFormTest

--- src/test/java/OnlinerFormTest.java
+++ src/test/java/OnlinerFormTest.java
@@ -5,20 +5,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class OnlinerFormTest {
 
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeMethod
+    public void init() {
+        driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "G:\\Installation\\chromedriver.exe");
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        driver.get("https://profile.onliner.by/registration");
+    }
+
     @Test
     public void checkRegistrationOnliner() {
-        WebDriver driver;
-        System.setProperty("webdriver.chrome.driver", "G:\\Installation\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-        driver.get("https://profile.onliner.by/registration");
 
 //        WebElement enterButton = driver.findElement(By.xpath("//div[@class=\"auth-bar__item auth-bar__item--text\"]"));
 //        enterButton.click();
@@ -38,10 +46,14 @@ public class OnlinerFormTest {
         WebElement submitButton = driver.findElement(By.xpath("//button[@type=\"submit\"]"));
         submitButton.click();
 
-        WebElement goToMailButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href=\"http://mail.onlcool.com/\"]")));
+        WebElement goToMailButton = wait.until(ExpectedConditions.elementToBeClickable(By.
+                xpath("//a[@href=\"http://mail.onlcool.com/\"]")));
 
         Assert.assertTrue(goToMailButton.getText().contains("Перейти в почту"));
+    }
 
+    @AfterMethod
+    public void tearDown() {
         driver.quit();
     }
 }
