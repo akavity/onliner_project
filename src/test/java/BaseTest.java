7a2e0732141cebed5595a83f419a0faf17e2b199
Basil Akavity <vakavity@gmail.com>
2023-06-06 11:58:57 +0300

    change annotations

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,16 +1,28 @@
 import org.example.driver.DriverManager;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+import java.util.ResourceBundle;
 
 public class BaseTest {
-
     private WebDriver driver;
-    @BeforeMethod
+    static ResourceBundle bundle = ResourceBundle.getBundle("onliner_framework");
+    private final String URL = bundle.getString("path_to_url");
+
+    @BeforeTest
     public void init() {
-       driver = DriverManager.getDriver();
+        driver = DriverManager.getDriver();
     }
-    @AfterMethod
+
+    @BeforeMethod
+    public void preparation() {
+        driver = DriverManager.getDriver();
+        driver.get(URL);
+    }
+
+    @AfterTest
     public void tearDown() {
         DriverManager.closeDriver();
     }
